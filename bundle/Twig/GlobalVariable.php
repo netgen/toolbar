ef2303c7cd89e1557137d3b70069e85b7df06e40
Edi Modrić <edi.modric@gmail.com>
2024-04-30 15:40:07 +0200

    Fix generating legacy admin UI links

--- bundle/Twig/GlobalVariable.php
+++ bundle/Twig/GlobalVariable.php
@@ -19,22 +19,25 @@ final class GlobalVariable
     public function __construct(
         private UrlGeneratorInterface $urlGenerator,
         private ConfigResolverInterface $configResolver,
-        private string $adminSiteAccessName,
+        private string $ibexaAdminSiteAccessName,
+        private string $legacyAdminSiteAccessName,
         private array $activatedBundles,
     ) {}
 
     public function getAdminUrlTemplate(): string
     {
+        $hasLegacyAdminUI = array_key_exists('NetgenAdminUIBundle', $this->activatedBundles);
+
         $baseUrl = $this->urlGenerator->generate(
             'ibexa.url.alias',
             [
                 'locationId' => $this->configResolver->getParameter('content.tree_root.location_id', null, 'default'),
-                'siteaccess' => $this->adminSiteAccessName,
+                'siteaccess' => $hasLegacyAdminUI ? $this->legacyAdminSiteAccessName : $this->ibexaAdminSiteAccessName,
             ],
             UrlGeneratorInterface::ABSOLUTE_URL,
         );
 
-        if (array_key_exists('NetgenAdminUIBundle', $this->activatedBundles)) {
+        if ($hasLegacyAdminUI) {
             return sprintf('%s/%s', trim($baseUrl, '/'), 'content/view/full/{locationId}');
         }
 
